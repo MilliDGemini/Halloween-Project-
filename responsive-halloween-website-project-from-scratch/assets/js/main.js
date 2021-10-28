e5844ca3380947547014eed00e392aba137e852b
millidgemini <fsumhyman@hotmail.com>
2021-10-28 15:39:59 -0500

    html and css done.

--- responsive-halloween-website-project-from-scratch/assets/js/main.js
+++ responsive-halloween-website-project-from-scratch/assets/js/main.js
@@ -1,22 +1,34 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== HOME SWIPER ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
